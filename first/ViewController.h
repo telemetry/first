4779e3b57697b8de0bc394e350d13fb1237f0d72
Terry Gillespie <terabyte@gmail.com>
2013-05-04 18:00:21 +1000

    First Commit
    iPhone Tutorials

--- first/ViewController.h
+++ first/ViewController.h
@@ -1,6 +1,6 @@
 //
 //  ViewController.h
-//  first
+//  first   
 //
 //  Created by Terry Gillespie on 26/03/12.
 //  Copyright (c) 2012 isotype. All rights reserved.
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController <UITextFieldDelegate>
+- (IBAction)changeGreeting:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (copy, nonatomic) NSString *userName;
 @end
